6f04877897937afba1205bbdf0b65536eeb6673e
OpymS <maisonneuve.menuisier@gmail.com>
2024-09-03 11:58:16 +0200

    gestion du retour après modification d'un lieu

--- assets/JavaScript/eventEdit.js
+++ assets/JavaScript/eventEdit.js
@@ -1,23 +1,53 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    // changement du lien d'édition du lieu en fonction du lieu sélectionné dans le select
-    const editLink = document.getElementById('place-edit');
-    document.getElementById('place-name').addEventListener('change', function(){
-        const value = this.value;
-        editLink.href=`/place/${value}/edit`;
+document.addEventListener("DOMContentLoaded", (e) => {
+    // OUverture et fermeture de la modale
+    let modal = null;
+
+    const openModal = (e) =>{
+        e.preventDefault();
+        const target = document.querySelector('.modal-container');
+        target.classList.remove('hidden');
+        target.removeAttribute('aria-hidden');
+        target.setAttribute('aria-modal', true);
+        document.querySelectorAll('.js-close-modal').forEach(btn => {
+            btn.addEventListener("click", closeModal);
+        })
+        setTimeout(()=>map.invalidateSize(),200);
+        modal = target;
+    }
+
+    const closeModal = (e) =>{
+        if (modal) {
+            e.preventDefault();
+            const target = document.querySelector('.modal-container');
+            target.classList.add('hidden');
+            target.setAttribute('aria-hidden', true);
+            target.removeAttribute('aria-modal');
+            document.querySelectorAll('.js-close-modal').forEach(btn => {
+                btn.removeEventListener("click", closeModal);
+            })
+            modal = null;
+        }
+    }
+
+    document.querySelectorAll(".js-modal").forEach(btn => {
+        btn.addEventListener("click", openModal);
+
     })
 
-    // stockage et récupération des données du formulaire lors de la création ou de la modification d'un lieu
-
+    // stockage et récupération des données du formulaire et de l'état de la modale lors de la création ou de la modification d'un lieu
     const destockDatas = () =>{
         if (sessionStorage.getItem('datas')){
-            datas=JSON.parse(sessionStorage.getItem("datas"));
-            console.log(datas);
+            const datas=JSON.parse(sessionStorage.getItem("datas"));
             document.getElementById("event_name").value=datas.name;
             document.getElementById("event_dateTimeStart").value=datas.dateTimeStart;
             document.getElementById("event_duration").value=datas.duration;
             document.getElementById("event_registrationDeadline").value=datas.registrationDeadline;
             document.getElementById("event_maxNbRegistration").value=datas.maxNbRegistration;
             document.getElementById("event_infoEvent").value=datas.infoEvent;
+           const modalStatus=datas.modalStatus;
+            if (modalStatus){
+                openModal(e);
+            }
             sessionStorage.removeItem("datas");
         }
     }
@@ -31,21 +61,76 @@ document.addEventListener("DOMContentLoaded", (event) => {
         const registrationDeadline = document.getElementById("event_registrationDeadline").value;
         const maxNbRegistration = document.getElementById("event_maxNbRegistration").value;
         const infoEvent = document.getElementById("event_infoEvent").value;
-        const datas = JSON.stringify({'name':name, 'dateTimeStart':dateTimeStart, 'duration':duration, 'registrationDeadLine':registrationDeadline,
-        'maxNbRegistration':maxNbRegistration, 'infoEvent':infoEvent})
+        const modalStatus = modal?true:false;
+        const datas = JSON.stringify({'name':name, 'dateTimeStart':dateTimeStart, 'duration':duration, 'registrationDeadline':registrationDeadline,
+        'maxNbRegistration':maxNbRegistration, 'infoEvent':infoEvent, 'modalStatus':modalStatus})
         sessionStorage.setItem('datas', datas);
     }
 
     const placeBtns = document.querySelectorAll(".js-place-btn");
     placeBtns.forEach((btn)=>{
-        btn.addEventListener("click", (event)=>{
-            event.preventDefault();
+        btn.addEventListener("click", (e)=>{
+            e.preventDefault();
             stockDatas();
-            window.location.href=event.target.href;
+            window.location.href=e.target.href;
+        });
+    })
+
+    // Affichage de la carte
+    let lat = 0;
+    let lon = 0;
+    const currentPlaceName = document.getElementById('place-name');
+    const currentPlace = document.getElementById('event_place')
+
+    let map = L.map("map",{
+        zoom: 10,
+        center:[lat,lon]
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        minZoom: 1,
+        maxZoom: 20,
+        attribution: 'données © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+
+    const datas = document.querySelector('.js-datas');
+    const places = JSON.parse(datas.getAttribute('data-places'));
+    const currentEvent = JSON.parse(datas.getAttribute('data-current-event'));
+
+    places.forEach((place) => {
+        lat = place.latitude;
+        lon = place.longitude;
+        if (currentPlaceName.value===place.name){
+            map.setView([lat,lon]);
+        }
+        let marker = L.marker([lat,lon]);
+        let popup = `<div>
+                                <div>
+                                    <h2 class="text-lg text-center">${place.name}</h2>
+                                    <p>${place.street}</p>
+                                    <p>${place.zipCode} ${place.city}</p>
+                                    <p><a href="/place/${place.id}/edit" class="bg-amber-400 rounded-md py-1 px-2 max-w-fit mx-auto hover:opacity-80 js-place-btn">Modifier</a> <button id="chose-place-${place.id}" class="bg-amber-400 rounded-md py-1 px-2 max-w-fit mx-auto hover:opacity-80">Sélectionner</button></p>
+                                </div>
+                            </div>`;
+        marker.bindPopup(popup).addTo(map);
+        marker.on('popupopen', function() {
+            document.getElementById(`chose-place-${place.id}`).addEventListener('click', (e) => {
+                e.preventDefault();
+                currentPlaceName.value = place.name;
+                currentEvent.place=place;
+                currentPlace.value=place.id;
+                console.log(currentEvent);
+                closeModal(e);
+            });
+            document.querySelector('.js-place-btn').addEventListener('click', (e)=>{
+                e.preventDefault();
+                stockDatas();
+                window.location.href=e.target.href;
+            })
         });
     })
 });
-
 
 
 
